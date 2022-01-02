923d8e87d39a0990e8839f7837b97a2608758e79
fanta <chunjoo0@naver.com>
2022-01-02 23:09:45 +0900

    Add sample project draft

---
+++ src/Loom.BuildingBlocks.Sample/ApiInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace Loom.BuildingBlocks.Sample
+{
+    public class ApiInfo : IApiInfo
+    {
+        public static IApiInfo Instance { get; private set; }
+        public static IApiInfo Instantiate(IConfiguration configuration)
+        {
+            Instance = new ApiInfo(configuration);
+
+            return Instance;
+        }
+
+        public ApiInfo(IConfiguration configuration)
+        {
+            AuthenticationAuthority = configuration["AuthenticationAuthority"];
+        }
+
+        public string AuthenticationAuthority { get; }
+
+        public string JwtBearerAudience => "sample-api";
+
+        public string Code => "sample-api";
+
+        public string Title => "Sample API";
+
+        public string Version => "v1";
+
+        public Assembly ApplicationAssembly => GetType().Assembly;
+
+        public SwaggerAuthInfo SwaggerAuthInfo => new SwaggerAuthInfo("sample-swagger-ui", "", "");
+
+        public IDictionary<string, string> Scopes => new Dictionary<string, string> { { "sample-api", Title } };
+    }
+}
